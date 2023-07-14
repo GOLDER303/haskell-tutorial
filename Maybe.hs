3e72ef1737861484af4af185215e2b034cc49bb0
GOLDER303 <szymonstepien@interia.pl>
2023-07-14 21:21:48 +0200

    Maybe Functions

--- Maybe.hs
+++ Maybe.hs
@@ -1,4 +1,14 @@
+import Data.Maybe
+
 -- data Maybe a = Just a | Nothing
 
 safeDiv :: Integral a => a -> a -> Maybe a
 safeDiv a b = if b == 0 then Nothing else Just $ div a b
+
+isSafeDivSuccess = isJust $ safeDiv 4 2
+
+isSafeDivNotSuccess = isNothing $ safeDiv 4 2
+
+safeDivValue = fromJust $ safeDiv 6 3
+
+safeDivValueWithDefault = fromMaybe 0 $ safeDiv 6 3
